66fe783272a6782f8105f54daa340493b84cb4d1
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-12-24 00:52:23 +0530

    Add slider

--- fourier/sketch.js
+++ fourier/sketch.js
@@ -1,25 +1,26 @@
 let time = 0;
 let wave = [];
+let slider;
 
 function setup() {
     createCanvas(600, 400);
+    slider = createSlider(1, 10, 1);
 }
 
 function draw() {
     background(0);
-    translate(200, 200);
+    translate(150, 200);
 
     let x = 0;
     let y = 0;
 
-    for(let i=0; i < 5; i++) {
+    for(let i=0; i < slider.value(); i++) {
         let prevx = x;
         let prevy = y;
         let n = i * 2 + 1;
-        let radius = 50 * (4 / (n * PI));
+        let radius = 75 * (4 / (n * PI));
         x += radius * cos(n * time);
         y += radius * sin(n * time);
-        wave.unshift(y); 
 
         stroke(255, 100);
         noFill();
@@ -28,6 +29,8 @@ function draw() {
         stroke(255);
         line(prevx, prevy, x, y);
     }
+
+    wave.unshift(y);
 
     translate(200, 0);
     line(x-200, y, 0, wave[0]);
