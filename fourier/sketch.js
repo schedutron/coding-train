e7bf1b17437ee9f1e16a27ad2590e6aad39ba07f
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-12-24 00:04:33 +0530

    Add moving point

--- fourier/sketch.js
+++ fourier/sketch.js
@@ -7,11 +7,16 @@ function setup() {
 function draw() {
     background(0);
     translate(200, 200);
-    let radius = 50;
+    let radius = 100;
 
     stroke(255);
     noFill();
     ellipse(0, 0, radius*2);
+
+    let x = radius * cos(time);
+    let y = radius * sin(time);
+    fill(255);
+    ellipse(x, y, 8);
 
     time += 0.01;
 }
