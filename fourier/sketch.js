0bf9aa3265be48f913e9ca08c7bdc60d43d773b2
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-12-24 00:44:05 +0530

    Generalize circles a bit

--- fourier/sketch.js
+++ fourier/sketch.js
@@ -1,4 +1,5 @@
 let time = 0;
+let wave = [];
 
 function setup() {
     createCanvas(600, 400);
@@ -7,17 +8,39 @@ function setup() {
 function draw() {
     background(0);
     translate(200, 200);
-    let radius = 100;
 
-    stroke(255);
+    let x = 0;
+    let y = 0;
+
+    for(let i=0; i < 5; i++) {
+        let prevx = x;
+        let prevy = y;
+        let n = i * 2 + 1;
+        let radius = 50 * (4 / (n * PI));
+        x += radius * cos(n * time);
+        y += radius * sin(n * time);
+        wave.unshift(y); 
+
+        stroke(255, 100);
+        noFill();
+        ellipse(prevx, prevy, radius*2);
+
+        stroke(255);
+        line(prevx, prevy, x, y);
+    }
+
+    translate(200, 0);
+    line(x-200, y, 0, wave[0]);
+    beginShape();
     noFill();
-    ellipse(0, 0, radius*2);
+    for(let i = 0; i < wave.length; i++) {
+        vertex(i, wave[i]); 
+    }
+    endShape();
 
-    let x = radius * cos(time);
-    let y = radius * sin(time);
-    fill(255);
-    line(0, 0, x, y);
-    ellipse(x, y, 8);
+    time += 0.025;
 
-    time -= 0.01;
+    if (wave.length > 250) {
+        wave.pop();
+    }
 }
